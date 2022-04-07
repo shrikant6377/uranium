1fb61ef1e9ba1947a585bd4f8d292a5d26bc6938
shrikant <shrikantkhare24@gmail.com>
2022-04-07 07:07:23 +0530

    third

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const today = require('../util/helper.js')
+//const express = require('express');
+//const today = require('../util/helper.js')
+const format = require('../validator/formatter.js')
 
 const router = express.Router();
-
+//problem1
 router.get('/test-me', function (req, res) {
-    console.log('I am inside the first route handler')
-    console.log('The endpoint value is', logger.endpoint)
+    console.log('Welcome to my application. I am shrikant and part of Functionup uranium.')
+    console.log('the end poit value is' logger.endpoint)
     console.log('Calling welcome')
     today.cur()
     res.send('my first ever api!')
 });
-
+//problem2
 router.get('/test-me2', function (req, res) {
     console.log('I am inside the second route handler')
     console.log("prints the current date: " + today.printDate())
@@ -20,12 +21,14 @@ router.get('/test-me2', function (req, res) {
     res.send('My second ever api!')
 });
 
-
-router.get('/test-me5', function (req, res) {
-    res.send('My final ever api!')
-});
-
+//problem3
 router.get('/test-me3', function (req, res) {
+    console.log()
+    res.send('i am shrikant'+'my third api!')
+    format.trimming();
+});
+//problem 4
+router.get('/test-me5', function (req, res) {
     res.send('My first ever api!')
 });
 
---
+++ src/validator/formatter.js
@@ -0,0 +1,11 @@
+
+const trim = function(){
+    let str="functionup"
+
+    let a = str.trim()
+    console.log("trime the string for white spaces:" + str.trim())
+
+  console.log("change the str to lowerCase :"+ str.toLowerCase())
+ console.log("change the str to UpperCase :"+ str.toUpperCase())
+}
+module.exports.trimming = trim;
